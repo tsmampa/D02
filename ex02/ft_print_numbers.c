6fc2a020cdfa7699074d36224ccd79b41e3d3176
Tshephang Mampa <tsmampa@c4r24s3.wethinkcode.co.za>
2020-07-09 15:26:40 +0200

    Print Nos

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,8 +6,31 @@
 /*   By: tsmampa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:47:40 by tsmampa           #+#    #+#             */
-/*   Updated: 2020/07/09 14:49:18 by tsmampa          ###   ########.fr       */
+/*   Updated: 2020/07/09 15:25:31 by tsmampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int ft_putchar(char ch)
+{
+	write(1, &ch, 1);
+		return(0);
+}
+
+void ft_print_numbers(void)
+{
+	char n;
+	n = '0';
+	while (n <= '9')
+	{
+		ft_putchar(n);
+		n = n + 1;
+	}
+}
+
+int main(void)
+{
+	ft_print_numbers();
+	return(0);
 
